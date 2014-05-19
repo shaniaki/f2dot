985210c85cc85429f3583495ac9df23d4c071aab
ugeorge <ugeorge@2f3b4d84-7048-4793-a15c-83de0941b73a>
2014-05-19 11:51:04 +0000

    added scripts installer command

--- f2dot/trunk/setup.py
+++ f2dot/trunk/setup.py
@@ -1,4 +1,40 @@
-from distutils.core import setup
+from distutils.core import setup, Command
+import os
+import sys
+import shutil
+
+
+class CopyScripts(Command):
+    description = "Creates a folder with just the scripts needed for running the program"
+    user_options = []
+    def initialize_options(self):
+		self.sourceDir = os.path.dirname(os.path.realpath(__file__))
+    def finalize_options(self):
+		self.destDir = os.getcwd()
+		print "Source scripts are found in", os.path.join(self.destDir,'scripts')
+    def run(self):
+		self.destDir = os.getcwd()
+		scriptsFolder = os.path.join(self.destDir,'scripts')
+		srcFolder = os.path.join(self.sourceDir,'src')
+		if not os.path.exists(scriptsFolder): 
+			os.makedirs(scriptsFolder)
+		src_files = os.listdir(srcFolder)
+		for file_name in src_files:
+			if file_name.endswith('.py'):
+				full_file_name = os.path.join(srcFolder, file_name)
+				if (os.path.isfile(full_file_name)):
+					shutil.copy(full_file_name, scriptsFolder)
+		xpathFolder = os.path.join(scriptsFolder,'xpath')
+		srcFolder = os.path.join(self.sourceDir,'xpath',)
+		if not os.path.exists(xpathFolder): 
+			os.makedirs(xpathFolder)
+		src_files = os.listdir(srcFolder)
+		for file_name in src_files:
+			if file_name.endswith('.py'):
+				full_file_name = os.path.join(srcFolder, file_name)
+				if (os.path.isfile(full_file_name)):
+					shutil.copy(full_file_name, xpathFolder)
+		
 
 setup(name = "f2dot",
 	version = "0.1.2",
@@ -9,6 +45,9 @@ setup(name = "f2dot",
 	packages = ['src','xpath'],
 	license = "BSD-3",
 	scripts = ["f2dot"],
-	install_requires=['pygraphviz',],
-	long_description = """Part of the ForSyDe (Formal System Design) suite, f2dot plots ForSyDe-XML intermedate model reppresentations to DOT graphs."""    
+	install_requires=['pygraphviz','libgraphviz-dev'],
+	long_description = """Part of the ForSyDe (Formal System Design) suite, f2dot plots ForSyDe-XML intermedate model reppresentations to DOT graphs."""    ,
+	cmdclass = {'scripts':CopyScripts},
 )
+
+
